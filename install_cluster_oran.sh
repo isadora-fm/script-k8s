9237ede38df966884ccb7da7fa8301ad7ad40e60
Isadora de Figueiredo Moreira <isadorafm@cpqd.com.br>
2023-09-15 11:46:31 -0300

    feat: update install docker

--- install_cluster_oran.sh
+++ install_cluster_oran.sh
@@ -43,7 +43,7 @@ function InstallDocker {
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
-    sudo apt install docker-ce=${DOCKER_VERSION}
+    sudo apt install -y docker-ce=${DOCKER_VERSION}
 }
 
 function PreInstallK8s {
